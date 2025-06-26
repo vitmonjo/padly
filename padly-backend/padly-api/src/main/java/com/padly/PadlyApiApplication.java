8000abf4ac367b842265af9c7a15a110c7563388
vitmonjo <vitmonjo@gmail.com>
2025-06-26 19:23:36 -0300

    add spring initial structure

---
+++ padly-backend/padly-api/src/main/java/com/padly/PadlyApiApplication.java
@@ -0,0 +1,13 @@
+package com.padly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PadlyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PadlyApiApplication.class, args);
+	}
+
+}
---
+++ padly-backend/padly-api/src/test/java/com/padly/PadlyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.padly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PadlyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
